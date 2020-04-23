4f6f9325f6b7c5655521c0a8d0555718ecb68d7a
Sanrro10 <i.glzsr@opendeusto.es>
2020-04-23 18:15:29 +0200

    Server route

--- src/main/java/es/deusto/spq/base/Main.java
+++ src/main/java/es/deusto/spq/base/Main.java
@@ -17,12 +17,12 @@ import ventanas.InicioSesion;
  */
 public class Main {
 	// Base URI the Grizzly HTTP server will listen on
-	public static final String BASE_URI = "http://localhost:8080/myapp/";
+	public static final String BASE_URI = "http://localhost:8080/biblioteca/";
 
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
 	 * application.
-	 * 
+	 *
 	 * @return Grizzly HTTP server.
 	 */
 	public static HttpServer startServer() {
@@ -37,7 +37,7 @@ public class Main {
 
 	/**
 	 * Main method.
-	 * 
+	 *
 	 * @param args
 	 * @throws IOException
 	 */
