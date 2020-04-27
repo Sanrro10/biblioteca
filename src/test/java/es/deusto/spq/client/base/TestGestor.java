74f0af6a2bb4523a74572be5bce8f78b597e7e21
Sanrro10 <i.glzsr@opendeusto.es>
2020-04-27 22:36:16 +0200

    Corrección de test

--- src/test/java/es/deusto/spq/client/base/TestGestor.java
+++ src/test/java/es/deusto/spq/client/base/TestGestor.java
@@ -10,16 +10,19 @@ public class TestGestor {
 
 	@Test
 	public void testGestor() {
-		Gestor gestor1= new Gestor(0, "admin@gmail.com", "admin", "admin", 654327811, "admin");
+		Gestor gestor1= new Gestor("admin@gmail.com", "admin", "admin", 654327811, "admin");
 		
-		int test = 0;
 		String email = "admin@gmail.com";
 		String nombre = "admin";
-		String Apellidos = "admin";
+		String apellidos = "admin";
 		int telefono = 654327811;
 		String contrasenya = "admin";
 		
-		assertTrue(gestor1.getCod_Usuario()==test);
+		assertTrue(gestor1.getEmail()==email);
+		assertTrue(gestor1.getNombre()==nombre);
+		assertTrue(gestor1.getApellidos()==apellidos);
+		assertTrue(gestor1.getTelefono()==telefono);
+		assertTrue(gestor1.getContrasenya()==contrasenya);
 		
 		
 	}
--- src/test/java/es/deusto/spq/client/base/TestUsuario.java
+++ src/test/java/es/deusto/spq/client/base/TestUsuario.java
@@ -11,16 +11,14 @@ public class TestUsuario {
 	@Test
 	public void testUsuario() {
 		
-		Usuario usuario1 = new Usuario(01, "a@a.com", "a", "a", 654321987, "123");
+		Usuario usuario1 = new Usuario("a@a.com", "a", "a", 654321987, "123");
 		
-		int cod_Usuario = 01;
 		String email = "a@a.com";
 		String nombre ="a";
 		String apellidos ="a";
 		int telefono =654321987;
 		String contrasenya = "123";
 		
-		assertTrue(usuario1.getCod_Usuario()==cod_Usuario);
 		assertTrue(usuario1.getEmail()==email);
 		assertTrue(usuario1.getNombre()==nombre);
 		assertTrue(usuario1.getApellidos()==apellidos);
