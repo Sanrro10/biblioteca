68d1cb85733522bf2ce78d7fe077b16f40558df7
kevin31000 <kevin.arnaiz@opendeusto.es>
2020-04-27 18:33:13 +0200

    LibroDTO + LibroAssembler

---
+++ src/main/java/es/deusto/spq/server/LibroAssembler.java
@@ -0,0 +1,30 @@
+package es.deusto.spq.server;
+
+import es.deusto.spq.base.Libro;
+
+public class LibroAssembler {
+	private static LibroAssembler instance;
+
+	private LibroAssembler() {
+	}
+
+	public static LibroAssembler getInstance() {
+		if (instance == null) {
+			instance = new LibroAssembler();
+		}
+
+		return instance;
+	}
+
+	public LibroDTO entityToDTO(Libro libro) {
+		LibroDTO dto = new LibroDTO();
+
+		dto.setCod_Libro(libro.getCod_Libro());
+		dto.setTitulo(libro.getTitulo());
+		dto.setAutor(libro.getAutor());
+		dto.setGenero(libro.getGenero());
+		dto.setReserva_Max(libro.getReserva_Max());
+
+		return dto;
+	}
+}
